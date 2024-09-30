83b4b3ba81f13746574d8417127b4a3e80ab153b
arthurwillame5 <arthurwillame2005@gmail.com>
2024-09-30 13:31:10 -0300

    chore: add cors config

--- src/functions/get-week-pending-goals.ts
+++ src/functions/get-week-pending-goals.ts
@@ -3,7 +3,6 @@ import { db } from "../db";
 import { goalCompletions, goals } from "../db/schema";
 import { and, count, eq, gte, lte, sql } from "drizzle-orm";
 
-
 export async function getWeekPendingGoals() {
   const firstDayOfWeek = dayjs().startOf("week").toDate();
   const lastDayOfWeek = dayjs().endOf("week").toDate();
